73954118a8eaee68a5490746acb08b69f123d829
Solarin-Johnson <solarinjohnson@gmail.com>
2025-08-20 10:38:01 +0100

    refactor: update tab layout and navigation structure, add new screens, and modify package dependencies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,20 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { NativeTabs, Icon, Label } from "expo-router/unstable-native-tabs";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Recent</Label>
+        <Icon sf="clock.fill" drawable="ic_history" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="shared">
+        <Icon sf="folder.fill.badge.person.crop" drawable="folder_shared" />
+        <Label>Shared</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="browse">
+        <Icon sf="folder.fill" drawable="folder" />
+        <Label>Browse</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
