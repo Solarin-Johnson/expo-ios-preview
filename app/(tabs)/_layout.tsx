f5102fea4799f25de3bc364581d91109ad1f7660
Solarin-Johnson <solarinjohnson@gmail.com>
2025-08-20 15:22:26 +0100

    refactor: enhance tab layout with animated view, update tab components, and remove unused files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,23 @@
 import { NativeTabs, Icon, Label } from "expo-router/unstable-native-tabs";
+import Animated from "react-native-reanimated";
 
 export default function TabLayout() {
   return (
-    <NativeTabs>
-      <NativeTabs.Trigger name="index">
-        <Label>Recent</Label>
-        <Icon sf="clock.fill" drawable="ic_history" />
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="shared">
-        <Icon sf="folder.fill.badge.person.crop" drawable="folder_shared" />
-        <Label>Shared</Label>
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="browse">
-        <Icon sf="folder.fill" drawable="folder" />
-        <Label>Browse</Label>
-      </NativeTabs.Trigger>
-    </NativeTabs>
+    <Animated.View style={{ flex: 1 }}>
+      <NativeTabs>
+        <NativeTabs.Trigger name="index">
+          <Label>Recent</Label>
+          <Icon sf="clock.fill" drawable="ic_history" />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="shared">
+          <Icon sf="folder.fill.badge.person.crop" drawable="folder_shared" />
+          <Label>Shared</Label>
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="browse">
+          <Icon sf="folder.fill" drawable="folder" />
+          <Label>Browse</Label>
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    </Animated.View>
   );
 }
--- app/(tabs)/browse.tsx
+++ app/(tabs)/browse.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { StyleSheet } from "react-native";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
 
-export default function Browse() {
+export default function Tab() {
   return (
-    <View>
-      <Text>Browse</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Browse Screen</ThemedText>
+    </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+});
